22cc46ce06b00d7521b066d8e518ff8215a69637
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-02 20:59:34 +0530

    firebase config codes added

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDfEtFCy89MJK6OTwUFO4PoSted5O65yuQ",
+  authDomain: "whether-station-a8898.firebaseapp.com",
+  projectId: "whether-station-a8898",
+  storageBucket: "whether-station-a8898.appspot.com",
+  messagingSenderId: "737862798825",
+  appId: "1:737862798825:web:b2b4bee2b7370ec70c0f78"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
