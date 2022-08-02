0717efdf402b8e2ab9662c05072dc5a02786f1be
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-02 21:31:26 +0530

    data reading ok with react project in hard coding of the firebase realtime databse

--- client/src/Design/Data.js
+++ client/src/Design/Data.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import { db } from "../firebase";
+import { uid } from "uid";
+import { onValue,ref } from "firebase/database";
 
 export default function Data() {
-  return (
-    <div>Data</div>
-  )
+  const [tem, setTem] = React.useState(0);
+  const [hum, setHum] = React.useState(0);
+  React.useEffect(() => {
+    onValue(ref(db), (snapshot) => {
+      const data = snapshot.val();
+      if (data !== null) {
+        setTem(data.tem);
+        setHum(data.hum);
+        // console.log(data);
+      }
+    });
+  }, []);
+  return <div>
+    {tem} + {hum}
+  </div>;
 }
