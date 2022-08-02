2baa30dfc97baeb14ad3d8aec3019a5edf9dd935
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-02 21:04:06 +0530

    install uid package for react project

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDfEtFCy89MJK6OTwUFO4PoSted5O65yuQ",
   authDomain: "whether-station-a8898.firebaseapp.com",
   projectId: "whether-station-a8898",
   storageBucket: "whether-station-a8898.appspot.com",
   messagingSenderId: "737862798825",
-  appId: "1:737862798825:web:b2b4bee2b7370ec70c0f78"
+  appId: "1:737862798825:web:b2b4bee2b7370ec70c0f78",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
